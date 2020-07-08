ad8364278092e39dde89ec870e379e3eb959b9ac
Dave Redmin <david.redmin@beta.dhs.gov>
2020-07-08 16:34:43 -0400

    Create locals needed to assume roles from remote state data.
    Create a local variable for the DNS account ID.

---
+++ locals.tf
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------------------------
+# Retrieve the effective Account ID, User ID, and ARN in which Terraform is
+# authorized.  This is used to calculate the session names for assumed roles.
+# ------------------------------------------------------------------------------
+data "aws_caller_identity" "current" {}
+
+# ------------------------------------------------------------------------------
+# Retrieve the information for all accounts in the organization.  This is used
+# to lookup the Users account ID for use in the assume role policy.
+# ------------------------------------------------------------------------------
+data "aws_organizations_organization" "cool" {
+  provider = aws.organizationsreadonly
+}
+
+# ------------------------------------------------------------------------------
+# Evaluate expressions for use throughout this configuration.
+# ------------------------------------------------------------------------------
+locals {
+  # Extract the user name of the current caller for use
+  # as assume role session names.
+  caller_user_name = split("/", data.aws_caller_identity.current.arn)[1]
+
+  # Find the DNS account ID by name
+  dns_account_id = [
+    for x in data.aws_organizations_organization.cool.accounts :
+    x.id if x.name == "DNS"
+  ][0]
+}
