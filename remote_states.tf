9a556b8524d6394de6a3238ab51e26d67d80438f
Dave Redmin <david.redmin@beta.dhs.gov>
2020-07-08 16:31:32 -0400

    Add remote states for DNS and Master accounts.

---
+++ remote_states.tf
@@ -0,0 +1,35 @@
+# ------------------------------------------------------------------------------
+# Retrieves state data from a Terraform backend. This allows use of the
+# root-level outputs of one or more Terraform configurations as input data
+# for this configuration.
+# ------------------------------------------------------------------------------
+
+data "terraform_remote_state" "dns" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/dns.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "master" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/master.tfstate"
+  }
+
+  workspace = "production"
+}
