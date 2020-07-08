689ba6413f0ee92c53c3ee463647bf4d33d5a7ea
Dave Redmin <david.redmin@beta.dhs.gov>
2020-07-08 16:35:04 -0400

    Remove old caller identity file.

--- caller_identity.tf
+++
@@ -1,6 +0,0 @@
-# ------------------------------------------------------------------------------
-# We can get the account ID of the DNS account from the provider's
-# caller identity.
-# ------------------------------------------------------------------------------
-data "aws_caller_identity" "dns" {
-}
