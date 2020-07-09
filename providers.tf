6b9bab30ce7dfd0faff84eb0872bdeb8e0606b0c
Dave Redmin <david.redmin@beta.dhs.gov>
2020-07-09 11:00:20 -0400

    Improve the accuracy of a comment- thanks @jsf9k for the suggestion!

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
-# This is the "default" provider that is used assume the roles in the other
-# providers.  It uses the credentials of the caller.  It is also used to
-# assume the roles required to access remote state in the Terraform backend.
+# This is the "default" provider that is used to obtain the caller's
+# credentials, which are used to set the session name when assuming roles in
+# the other providers.
 
 provider "aws" {
   region = var.aws_region
